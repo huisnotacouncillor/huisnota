adbfa1982246f7f85031e6898a75333e060134f8
huzhuangzhuang <zhuangzhuang@dao42.com>
2024-05-08 12:11:52 +0800

    feat: second commit

--- src/content/pages/about.md
+++ src/content/pages/about.md
@@ -9,5 +9,3 @@ I am a web developer working with tech stack `Vue` `Nuxt` `React` `NextJs` etc..
 Currently focus on a CDE product with `React` `NextJS`.
 
 Besides coding, I enjoy climbing and hiking; and occasionally running (maybe twice a week).
-
-kor
--- src/site-config.ts
+++ src/site-config.ts
@@ -47,57 +47,57 @@ export const siteConfig = {
     },
   ],
   projects: [
-    {
-      title: 'Develop Templates',
-      projects: [
-        {
-          text: 'Frosty Web',
-          description: 'A clean and minimalist website template designed to showcase content with style.',
-          icon: 'i-carbon-webhook',
-          href: '',
-        },
-      ],
-    },
-    {
-      title: 'Framework',
-      projects: [
-        {
-          text: 'Pixel Craft',
-          description: 'Frontend framework for crafting pixel-perfect web applications with a responsive design.',
-          icon: 'i-carbon-pen-fountain',
-          href: '',
-        },
-        {
-          text: 'Aurora UI',
-          description: 'Modern UI library designed to streamline frontend development with modular components.',
-          icon: 'i-carbon-mountain',
-          href: '',
-        },
-        {
-          text: 'Nimbus CSS',
-          description: 'Lightweight CSS framework for building responsive websites with a flexible grid system.',
-          icon: 'i-carbon-face-satisfied',
-          href: '',
-        },
-      ],
-    },
-    {
-      title: 'Library',
-      projects: [
-        {
-          text: 'Zenith Scroll',
-          description: 'Smooth-scrolling JavaScript library for creating immersive scrolling experiences.',
-          icon: '',
-          href: '',
-        },
-        {
-          text: 'Polaris JS',
-          description: 'Lightweight JavaScript library for creating smooth animations and transitions.',
-          icon: 'i-carbon-tools-alt',
-          href: '',
-        },
-      ],
-    },
+    // {
+    //   title: 'Develop Templates',
+    //   projects: [
+    //     {
+    //       text: 'Frosty Web',
+    //       description: 'A clean and minimalist website template designed to showcase content with style.',
+    //       icon: 'i-carbon-webhook',
+    //       href: '',
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: 'Framework',
+    //   projects: [
+    //     {
+    //       text: 'Pixel Craft',
+    //       description: 'Frontend framework for crafting pixel-perfect web applications with a responsive design.',
+    //       icon: 'i-carbon-pen-fountain',
+    //       href: '',
+    //     },
+    //     {
+    //       text: 'Aurora UI',
+    //       description: 'Modern UI library designed to streamline frontend development with modular components.',
+    //       icon: 'i-carbon-mountain',
+    //       href: '',
+    //     },
+    //     {
+    //       text: 'Nimbus CSS',
+    //       description: 'Lightweight CSS framework for building responsive websites with a flexible grid system.',
+    //       icon: 'i-carbon-face-satisfied',
+    //       href: '',
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: 'Library',
+    //   projects: [
+    //     {
+    //       text: 'Zenith Scroll',
+    //       description: 'Smooth-scrolling JavaScript library for creating immersive scrolling experiences.',
+    //       icon: '',
+    //       href: '',
+    //     },
+    //     {
+    //       text: 'Polaris JS',
+    //       description: 'Lightweight JavaScript library for creating smooth animations and transitions.',
+    //       icon: 'i-carbon-tools-alt',
+    //       href: '',
+    //     },
+    //   ],
+    // },
   ],
   footerNavLinks: [
     {
